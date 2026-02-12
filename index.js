d440e08842a7058238da2adbe233b178d4eb7c72
surya2156 <suryagoayl156@gmail.com>
2026-02-12 15:19:04 +0530

    okk

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 const http = require('http');
 
 const myserver = http.createServer((req, res) => {
-  console.log('server 1');
 
-  if (req.url === '/about') {
-    res.end('this is about page');  //router
+  if (req.url === '/') {
+   
+    res.end(`
+      <html>
+        <head>
+          <title>Home</title>
+        </head>
+        <body>
+          <h1>Home Page</h1>            
+          <h2>ABES Engineering College</h2>
+          <a href="/about">About</a><br>
+          <a href="/contact">Contact</a>
+        </body>
+      </html>
+    `);
   }
-  else if (req.url === '/aboutus') {
-    res.end('this is about us page');  //router
+
+  else if (req.url === '/about') {
+    // ABOUT PAGE WITH IMAGE
+    res.end(`
+      <html>
+        <head>
+          <title>About</title>
+        </head>
+        <body>
+          <h1>About Page</h1>
+          <p>This page is about ABES Engineering College.</p>
+          <img src="./image copy.png" width="400"/>
+          <br><br>
+          <a href="/">Home</a>
+        </body>
+      </html>
+    `);
   }
+
+  else if (req.url === '/contact') {
+    // CONTACT PAGE
+    res.end(`
+      <html>
+        <head>
+          <title>Contact</title>
+        </head>
+        <body>
+          <h1>Contact Page</h1>
+          <p>Email: info@abes.ac.in</p>
+          <p>Phone: 0120-7135112</p>
+          <a href="/">Home</a>
+        </body>
+      </html>
+    `);
+  }
+
   else {
-    res.end('hello world');
+    res.end("404 Page Not Found");
   }
 });
 
-myserver.listen(8000, () => {
-  console.log('server running on port 8000');  //port number
+myserver.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
+
                   
